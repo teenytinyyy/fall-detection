f1011b64725ed1601db7ae30f9da1407a305e3c8
Zzznorlax <tsuting.yu@gmail.com>
2022-07-01 23:16:58 +0800

    feat(generate_lebel): make label visualization match when number of images and labels not matching

--- generate_label.py
+++ generate_label.py
@@ -8,10 +8,23 @@ from utils import files as file_utils
 
 
 if __name__ == '__main__':
-    data_dir = "dataset/json_data (1_1)"
+    data_dir = "dataset/json_data (1)"
     json_files = file_utils.get_sorted_files(data_dir, ext=[".json"])
+    img_files = file_utils.get_sorted_files(data_dir, ext=[".jpg"])
+    labeled_files = []
 
     json_data_list = []
+    img_list = []
+
+    json_map = {}
+    for json_path in json_files:
+        json_idx, _ = file_utils.get_extension(file_utils.get_filename(json_path))
+        json_map[json_idx] = True
+
+    for img_path in img_files:
+        img_idx, _ = file_utils.get_extension(file_utils.get_filename(img_path))
+        if img_idx in json_map:
+            labeled_files.append(img_path)
 
     for file_path in json_files:
         with open(file_path, "r") as reader:
@@ -19,7 +32,9 @@ if __name__ == '__main__':
             points = [[point[1], point[0]] for point in data['shapes'][0]['points']]
             json_data_list.append(points)
 
-    img_list = img_utils.read_from_folder(data_dir)
+    for img_path in labeled_files:
+        img = cv2.imread(img_path)
+        img_list.append(img)
 
     for idx, frame in enumerate(img_list):
 
@@ -31,15 +46,15 @@ if __name__ == '__main__':
 
         # print(angle, width, height, center)
 
-        img_utils.draw_dot(frame, center[1], center[0], radius=7, color=(255, 255, 0))
+        img_utils.draw_dot(frame, center[1], center[0], radius=2, color=(255, 255, 0))
 
         for point in corner_points:
-            img_utils.draw_dot(frame, point[1], point[0], radius=5, color=(255, 255, 0))
+            img_utils.draw_dot(frame, point[1], point[0], radius=2, color=(255, 255, 0))
 
         for point in json_data_list[idx]:
             img_utils.draw_dot(frame, point[1], point[0], radius=2, color=(255, 255, 0))
 
-        cv2.ellipse(frame, (center[1], center[0]), (int(height), int(width)), -angle, 0, 360, (255, 255, 0), 1)
+        cv2.ellipse(frame, (center[1], center[0]), (int(height), int(width)), -angle, 0, 360, (255, 255, 0), 5)
 
         cv2.imshow("image sequence", frame)
 
